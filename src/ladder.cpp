d280b2364fed9b5e8365c80924c26a2dd504d5a9
EliranAvigani <eavigani22@gmail.com>
2025-03-15 23:49:32 +0000

    update

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -51,6 +51,12 @@ bool edit_distance_within(const std::string &str1, const std::string &str2, int 
     return false;
 }
 bool is_adjacent(const string& word1, const string& word2) {
+    size_t len1 = word1.length();
+    size_t len2 = word2.length();
+    if (!(len1 == len2 || abs(static_cast<int>(len1 - len2)) == 1)) {
+        return false;
+    }
+    
     return edit_distance_within(word1, word2, 1);
 }
 vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list) {
